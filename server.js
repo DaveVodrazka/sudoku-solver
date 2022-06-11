aa82808d5837475a2c3f2e20db76e2639bc11d59
Vodrazka, David <david.vodrazka@firma.seznam.cz>
2022-06-11 19:53:38 +0200

    chore: move public to docs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('docs'));
 
 app.listen(port, () => {
   console.log(`Sudoku solver running at http://localhost:${port}`)
